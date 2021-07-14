dcf6135f31d6e986de1976e2e9568de5ed8dc91e
itachi176 <lhoang17062000@gmail.com>
2021-07-15 01:14:25 +0700

    complete

--- tool.py
+++ tool.py
@@ -2,57 +2,49 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from time import sleep
 
+def scroll_page():
+    for i in range(7):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        sleep(3)
 
-def scrape_all_imgs_google(searchTerm):
-    def scroll_page():
-        for i in range(7):
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            sleep(3)
+def click_button():
+    more_imgs_button_xpath = '/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div[1]/div[2]/div[2]/input'
+    driver.find_element_by_xpath(more_imgs_button_xpath).click()   
 
-    def click_button():
-        more_imgs_button_xpath = '/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div[1]/div[2]/div[2]/input'
-        driver.find_element_by_xpath(more_imgs_button_xpath).click()   
+def find_images():  
+    html_source = driver.page_source
+    soup = BeautifulSoup(html_source, 'html.parser')
 
-    # def create_soup():
-       
-    def find_imgs():
-        html_source = driver.page_source
-        soup = BeautifulSoup(html_source, 'html.parser')
+    imgs_urls = []
+    for img in soup.find_all('img'):
+        try:
+            if img['src'].startswith('http'):
+                imgs_urls.append(img['src'])
+        except:
+            pass 
 
-        imgs_urls = []
-        for img in soup.find_all('img'):
-            try:
-                if img['src'].startswith('http'):
-                    imgs_urls.append(img['src'])
-            except:
-                pass
+    return imgs_urls
 
-    #create webdriver
-    driver = webdriver.Chrome(executable_path="./driver/chromedriver")
+#create webdriver
+driver = webdriver.Chrome(executable_path="./driver/chromedriver")
 
-    #define url using search term
-    searchUrl = "https://www.google.com/search?q={}&site=webhp&tbm=isch".format(searchTerm)
+#define url 
+url = "https://www.google.com/search?q={}&site=webhp&tbm=isch".format("computer")
 
-    #get url
-    driver.get(searchUrl)
+#get url 
+driver.get(url)
 
-    try:
-        click_button()
-        scroll_page()
-    except:
-        scroll_page()
-        click_button()   
+#keo den khi nao nhan vao nut them anh 
+scroll_page()
+click_button()
+#tim anh 
+img_urls = find_images()
 
-    # #create soup only after we loaded all imgs when we scroll'ed the page down
-    # create_soup()
+#dong driver
+driver.close()
 
-    #find imgs in soup
-    find_imgs()
+print(img_urls)
 
-    #close driver
-    driver.close()
 
-    #return list of all img urls found in page
 
-urls = scrape_all_imgs_google('computer')
-print(len(urls))
+
